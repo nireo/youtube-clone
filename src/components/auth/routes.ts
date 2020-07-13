f168237034d49f4553e8711dffefbf95ad58e936
nireo <eemil.lehtikangas1@gmail.com>
2020-07-13 14:23:14 +0300

    added a login route

--- src/components/auth/routes.ts
+++ src/components/auth/routes.ts
@@ -16,8 +16,33 @@ router.post('/register', (req: express.Request, res: express.Response) => {
         id: uuidv4(),
       });
 
-      res.status(200).json({ data: user });
+      res.status(200).json(user);
     });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/login', async (req: express.Request, res: express.Response) => {
+  try {
+    const user: any = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    const passwordMatches = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatches) {
+      return res.status(401).json({ message: 'Credentials do not match.' });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error);
   }
