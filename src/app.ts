ef0c0f205dbacf2c838533161187376277dfed97
nireo <eemil.lehtikangas1@gmail.com>
2020-07-13 15:10:48 +0300

    added a comment create route and delete route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import videoRoutes from './components/videos/routes';
 import authRoutes from './components/auth/routes';
 import userRoutes from './components/users/routes';
+import commentRouter from './components/comments/routes';
 
 const PORT: number = 3001;
 const app: express.Application = express();
@@ -24,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/videos', videoRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/comments', commentRouter);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
--- src/components/comments/routes.ts
+++ src/components/comments/routes.ts
@@ -1,5 +1,49 @@
 import express from 'express';
+import { Video, Comment } from '../../sequelize';
+import { v4 as uuidv4 } from 'uuid';
 
 const router: express.Router = express.Router();
+
+router.post(
+  '/:videoId',
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const video = await Video.findOne({ where: { id: req.params.videoId } });
+      if (!video) {
+        return res.status(404).json({ message: 'Video not found.' });
+      }
+
+      const newComment = new Comment({
+        id: uuidv4(),
+        content: req.body.content,
+        videoId: req.params.videoId,
+      });
+
+      await newComment.save();
+      res.status(200).json(newComment);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+);
+
+router.delete(
+  '/:commentId',
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const comment = await Comment.findOne({
+        where: { id: req.params.commentId },
+      });
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found.' });
+      }
+
+      await comment.destroy();
+      res.status(204);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+);
 
 export default router;
