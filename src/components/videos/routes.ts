8cae4e5436191bcc05acfae9b87e6f9d89aa1d6b
nireo <eemil.lehtikangas1@gmail.com>
2020-07-14 00:37:52 +0300

    added a video delete route

--- src/components/videos/routes.ts
+++ src/components/videos/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { Video } from '../../sequelize';
 import authenticateToken from '../../middlewares/tokenAuth';
+import fs from 'fs';
 
 const router: express.Router = express.Router();
 
@@ -37,5 +38,34 @@ router.post('/', authenticateToken, async (req: any, res: express.Response) => {
     res.status(500).send(error);
   }
 });
+
+router.delete(
+  '/:videoId',
+  authenticateToken,
+  async (req: any, res: express.Response) => {
+    try {
+      const video: any = await Video.findOne({
+        where: { id: req.body.videoId },
+      });
+      if (!video) {
+        res.status(403).json({ message: 'Video not found' });
+      }
+
+      // check user ownership
+      if (req.user.id != video.userId) {
+        return res.status(401).json({ message: 'Forbidden' });
+      }
+
+      // remove the video file from the file-system
+      fs.unlink(`./videos/${req.body.videoId}`, (err) => {
+        if (err) return res.status(500).json({ message: err });
+      });
+
+      res.status(204);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 
 export default router;
