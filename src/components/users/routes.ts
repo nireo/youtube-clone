a49d845ab790ab06b21a2e947e55863a5c054435
nireo <eemil.lehtikangas1@gmail.com>
2020-07-14 15:16:47 +0300

    added get user videos route

--- src/components/users/routes.ts
+++ src/components/users/routes.ts
@@ -1,8 +1,33 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { User } from '../../sequelize';
+import { Video } from '../../sequelize';
 
 const router: express.Router = express.Router();
+
+// THE USER COMPONENT IS BASICALLY THE CHANNEL COMPONENT
+router.get(
+  '/videos/:userId',
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const user = await User.findOne({ where: { id: req.params.userId } });
+      if (!user) {
+        return res.status(404).json({ message: 'That user does not exist' });
+      }
+
+      const videos = await Video.findAll({
+        where: { userId: req.params.userId },
+      });
+      if (!videos) {
+        return res.status(404).json({ message: 'No videos found' });
+      }
+
+      res.json(videos);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+);
 
 router.post('/avatar', async (req: express.Request, res: express.Response) => {
   try {
