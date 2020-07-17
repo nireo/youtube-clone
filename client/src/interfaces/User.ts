e57b908fe443762223d13f51d6f6aa0c19f5d873
nireo <eemil.lehtikangas1@gmail.com>
2020-07-18 01:30:52 +0300

    added the user reducer

--- client/src/interfaces/User.ts
+++ client/src/interfaces/User.ts
@@ -2,3 +2,10 @@ export interface Credentials {
   username: string;
   password: string;
 }
+
+export interface User {
+  id: string;
+  username: string;
+  avatar: string | null;
+  createdAt: string;
+}
--- client/src/store/userReducer.ts
+++ client/src/store/userReducer.ts
@@ -1,8 +1,41 @@
-const reducer = (state: object | null = null, action: any) => {
+import { User, Credentials } from "../interfaces/User";
+import { Dispatch } from "redux";
+import { login, register } from "../services/auth";
+
+const reducer = (state: User | null = null, action: any) => {
   switch (action.type) {
+    case "LOGIN":
+      return action.data;
+    case "LOGOUT":
+      return null;
     default:
       return state;
   }
+};
+
+export const loginAction = (credentials: Credentials) => {
+  return async (dispatch: Dispatch) => {
+    const user = await login(credentials);
+    dispatch({
+      type: "LOGIN",
+      data: user
+    });
+  };
+};
+
+export const registerAction = (credentials: Credentials) => {
+  return async (dispatch: Dispatch) => {
+    const user = await register(credentials);
+    dispatch({
+      type: "LOGIN",
+      data: user
+    });
+  };
+};
+
+export const logoutAction = () => {
+  localStorage.clear();
+  return { type: "LOGOUT" };
 };
 
 export default reducer;
