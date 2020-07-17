e45e2f5e2a6ab0012dc485c112a99df21d48221c
nireo <eemil.lehtikangas1@gmail.com>
2020-07-17 16:25:16 +0300

    hosted static files with express

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import fileUpload from 'express-fileupload';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import fileUpload from "express-fileupload";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import videoRoutes from './components/videos/routes';
-import authRoutes from './components/auth/routes';
-import userRoutes from './components/users/routes';
-import commentRouter from './components/comments/routes';
+import videoRoutes from "./components/videos/routes";
+import authRoutes from "./components/auth/routes";
+import userRoutes from "./components/users/routes";
+import commentRouter from "./components/comments/routes";
 
 const app: express.Application = express();
 dotenv.config();
@@ -15,7 +15,7 @@ dotenv.config();
 // enable file upload
 app.use(
   fileUpload({
-    createParentPath: true,
+    createParentPath: true
   })
 );
 
@@ -23,9 +23,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/videos', videoRoutes);
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/comments', commentRouter);
+app.use("/video", express.static("./videos"));
+app.use("/avatar", express.static("./avatars"));
+
+app.use("/videos", videoRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/comments", commentRouter);
 
 export default app;
