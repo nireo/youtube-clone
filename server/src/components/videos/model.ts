931e0e033d3e35917c7a8c341578e271d650063b
nireo <eemil.lehtikangas1@gmail.com>
2020-07-17 22:38:53 +0300

    added a thumbnails to videos

---
+++ server/src/components/videos/model.ts
@@ -0,0 +1,36 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const VideoModel = (sequelize: Sequelize) => {
+  return sequelize.define("Video", {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    fileExtension: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    dislikes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    thumbnail: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+};
+
+export default VideoModel;
