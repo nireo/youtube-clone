e6c14193d203d5b067c4b802d5808aac5db35df8
nireo <eemil.lehtikangas1@gmail.com>
2020-07-18 13:52:06 +0300

    added a comment service component

---
+++ client/src/interfaces/Comment.ts
@@ -0,0 +1,7 @@
+export interface Comment {
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  edited: boolean;
+}
---
+++ client/src/services/comment.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { CreateComment, RateComment } from "../interfaces/Comment";
+const baseUrl: string = "/comments";
+
+let token: string | null = null;
+
+export const setToken = (newToken: string) => {
+  token = `bearer ${newToken}`;
+};
+
+const getConfig = () => ({
+  headers: {
+    Authorization: token
+  }
+});
+
+export const createComment = async (
+  videoId: string,
+  comment: CreateComment
+) => {
+  const response = await axios.post(
+    `${baseUrl}${videoId}`,
+    comment,
+    getConfig()
+  );
+  return response.data;
+};
+
+export const getComments = async (videoId: string) => {
+  const response = await axios.get(`${baseUrl}/${videoId}`);
+  return response.data;
+};
+
+export const rateComment = async (action: RateComment, commentId: string) => {
+  const response = await axios.patch(
+    `${baseUrl}/${action}/${commentId}`,
+    getConfig()
+  );
+  return response.data;
+};
+
+export const updateComment = async (
+  commentId: string,
+  updatedComment: CreateComment
+) => {
+  const response = await axios.patch(
+    `${baseUrl}/${commentId}`,
+    updatedComment,
+    getConfig()
+  );
+  return response.data;
+};
