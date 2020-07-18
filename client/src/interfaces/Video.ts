53824a706bc9a1d4bf067eb14dc1667b6b9f76db
nireo <eemil.lehtikangas1@gmail.com>
2020-07-18 14:22:43 +0300

    added a video reducer

--- client/src/interfaces/Video.ts
+++ client/src/interfaces/Video.ts
@@ -7,3 +7,16 @@ export enum RateActions {
   Dislike = "dislike",
   Like = "like"
 }
+
+export interface Video {
+  id: string;
+  title: string;
+  description: string;
+  fileExtension: string;
+  likes: number;
+  dislikes: number;
+  thumbnail: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+}
