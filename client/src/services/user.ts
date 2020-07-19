011f4428f066a20b368bebd21fef75bfae2b7e77
nireo <eemil.lehtikangas1@gmail.com>
2020-07-19 16:39:41 +0300

    added user service

---
+++ client/src/services/user.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+const baseUrl: string = "/users";
+
+let token: string | null = null;
+export const setToken = (newToken: string) => {
+  token = `bearer ${newToken}`;
+};
+
+const getConfig = () => ({
+  headers: {
+    Authorization: token
+  }
+});
+
+export const getUserVideos = async (userId: string) => {
+  const response = await axios.get(`${baseUrl}/${userId}`);
+  return response.data;
+};
+
+export const getAllUsers = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+// doesn't need id since it uses the token
+export const deleteUser = async () => {
+  const response = await axios.delete(baseUrl, getConfig());
+  return response.data;
+};
+
+export const subscribeToUser = async (userId: string) => {
+  const response = await axios.post(
+    `${baseUrl}/subscribe/${userId}`,
+    getConfig()
+  );
+  return response.data;
+};
+
+export const unsubscribeUser = async (userId: string) => {
+  const response = await axios.delete(
+    `${baseUrl}/subscribe/${userId}`,
+    getConfig()
+  );
+  return response.data;
+};
+
+export const getSubscriptions = async () => {
+  const response = await axios.get(`${baseUrl}/subscription`, getConfig());
+  return response.data;
+};
