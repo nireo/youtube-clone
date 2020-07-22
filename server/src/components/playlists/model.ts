d8cbaafade172b7fc77e72a8e4a624381a5ff4f7
nireo <eemil.lehtikangas1@gmail.com>
2020-07-22 21:26:34 +0300

    added a playlist model

---
+++ server/src/components/playlists/model.ts
@@ -0,0 +1,25 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const PlaylistModel = (sequelize: Sequelize) => {
+  return sequelize.define("Playlist", {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      defaultValue: "No description provided"
+    },
+    videos: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: []
+    }
+  });
+};
+
+export default PlaylistModel;
--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -1,20 +1,20 @@
-import { Sequelize } from 'sequelize';
-import VideoModel from './components/videos/model';
-import UserModel from './components/users/model';
-import CommentModel from './components/comments/model';
-import SubscriptionModel from './components/users/subscribeModel';
-import CommentLikeModel from './components/comments/commentLikeModel';
-import VideoLikeModel from './components/videos/videoLikeModel';
+import { Sequelize } from "sequelize";
+import VideoModel from "./components/videos/model";
+import UserModel from "./components/users/model";
+import CommentModel from "./components/comments/model";
+import SubscriptionModel from "./components/users/subscribeModel";
+import CommentLikeModel from "./components/comments/commentLikeModel";
+import VideoLikeModel from "./components/videos/videoLikeModel";
 
-let database = 'youtube';
-if (process.env.NODE_ENV === 'test') {
-  database = 'youtubetest';
+let database = "youtube";
+if (process.env.NODE_ENV === "test") {
+  database = "youtubetest";
 }
 
-export const sequelize = new Sequelize(database, 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
+export const sequelize = new Sequelize(database, "postgres", "password", {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false
 });
 (async () => await sequelize.sync({ alter: true }))();
 
@@ -24,8 +24,10 @@ export const Comment = CommentModel(sequelize);
 export const Subscription = SubscriptionModel(sequelize);
 export const CommentLike = CommentLikeModel(sequelize);
 export const VideoLike = VideoLikeModel(sequelize);
+export const Playlist = PlaylistModel(sequelize);
 
 // associations
-Video.belongsTo(User, { foreignKey: 'userId' });
-Video.hasMany(Comment, { foreignKey: 'videoId' });
-User.hasMany(Comment, { foreignKey: 'userId' });
+Video.belongsTo(User, { foreignKey: "userId" });
+Video.hasMany(Comment, { foreignKey: "videoId" });
+User.hasMany(Comment, { foreignKey: "userId" });
+Playlist.belongsTo(User, { foreignKey: "userId" });
