57976ad59cd0add6ed4c6556c3258548e78b8a03
nireo <eemil.lehtikangas1@gmail.com>
2020-07-25 14:32:42 +0300

    added notification routes

--- server/src/components/notifications/routes.ts
+++ server/src/components/notifications/routes.ts
@@ -1,5 +1,63 @@
 import express from "express";
+import authenticateToken from "../../middlewares/tokenAuth";
+import { Notification } from "../../sequelize";
 
 const router: express.Router = express.Router();
+
+router.patch(
+  "/:notificationId",
+  authenticateToken,
+  async (req: any, res: express.Response) => {
+    try {
+      const { notificationId } = req.params;
+
+      const notification: any = await Notification.findOne({
+        where: { id: notificationId }
+      });
+
+      if (!notification) {
+        return res.status(404);
+      }
+
+      if (notification.userId !== req.user.id) {
+        return res.status(403);
+      }
+
+      notification.read = true;
+
+      await notification.save();
+      res.status(204);
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  }
+);
+
+router.delete(
+  "/:notificationId",
+  authenticateToken,
+  async (req: any, res: express.Response) => {
+    try {
+      const { notificationId } = req.params;
+
+      const notification: any = await Notification.findOne({
+        where: { id: notificationId }
+      });
+
+      if (!notification) {
+        return res.status(404);
+      }
+
+      if (notification.userId !== req.user.id) {
+        return res.status(403);
+      }
+
+      await notification.destroy();
+      res.status(204);
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  }
+);
 
 export default router;
