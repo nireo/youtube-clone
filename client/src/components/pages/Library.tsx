7b54146b2ab87f4f3bab15190e8cfcd85d5f3e94
nireo <eemil.lehtikangas1@gmail.com>
2020-08-01 17:33:54 +0300

    improved the library page

--- client/src/components/pages/Library.tsx
+++ client/src/components/pages/Library.tsx
@@ -6,9 +6,9 @@ import { User } from "../../interfaces/User";
 import { Redirect } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import { Video } from "../../interfaces/Video";
-import { VideoEntryFull } from "../other/VideoEntryFull";
 import { getLibraryData } from "../../services/user";
 import { Helmet } from "react-helmet";
+import { VideoEntrySmall } from "../other/VideoEntrySmall";
 
 type Props = {
   user: User | null;
@@ -45,15 +45,40 @@ const Library: React.FC<Props> = ({ user }) => {
       <Typography variant="h5" style={{ marginTop: "2rem" }}>
         History
       </Typography>
-      {historyVideos.map((video: Video) => (
-        <VideoEntryFull key={`history-${video.id}`} video={video} />
-      ))}
+      {historyVideos.length === 0 ? (
+        <div>
+          <Typography style={{ marginTop: "1rem" }} variant="h6">
+            No history.
+          </Typography>
+          <Typography>
+            You're history site will be filled with more data, when you watch
+            videos.
+          </Typography>
+        </div>
+      ) : (
+        <div>
+          {historyVideos.map((video: Video) => (
+            <VideoEntrySmall key={`history-${video.id}`} video={video} />
+          ))}
+        </div>
+      )}
       <Typography variant="h5" style={{ marginTop: "2rem" }}>
         Watch later
       </Typography>
-      {watchLaterVideos.map((video: Video) => (
-        <VideoEntryFull key={`watchLater-${video.id}`} video={video} />
-      ))}
+      {watchLaterVideos.length === 0 ? (
+        <div>
+          <Typography variant="h6">No watch later videos.</Typography>
+          <Typography>
+            You don't have any videos in the watch later list.
+          </Typography>
+        </div>
+      ) : (
+        <div>
+          {watchLaterVideos.map((video: Video) => (
+            <VideoEntrySmall key={`watchLater-${video.id}`} video={video} />
+          ))}
+        </div>
+      )}
     </Container>
   );
 };
