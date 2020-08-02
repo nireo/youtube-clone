96b4677cb7b3e78e084adc137cac6644ab2e698b
nireo <eemil.lehtikangas1@gmail.com>
2020-08-02 17:09:08 +0300

    added all the community tests

--- server/src/components/community/community.test.ts
+++ server/src/components/community/community.test.ts
@@ -16,25 +16,91 @@ beforeAll(async () => {
   testUser = newTestUser;
 });
 
-describe("Community post creation", () => {
+describe("Community routes", () => {
+  let postId: string | null = null;
   test("User can create a post", async () => {
     if (testUser === null) {
       // test user cannot be created so return that the test failed
       expect(false).toBeTruthy();
       return;
     } else {
+      const data = {
+        content: "This is a post"
+      };
+
+      const response = await api
+        .post("/community")
+        .set("Authorization", `bearer ${testUser.token}`)
+        .send(data)
+        .expect(200);
+
+      expect(response.body.content).toBe("This is a post");
+      postId = response.body.content;
     }
-    const data = {
-      content: "This is a post"
-    };
+  });
 
-    const response = await api
-      .post("/community")
-      .set("Authorization", `bearer ${testUser.token}`)
-      .send(data)
-      .expect(200);
+  test("A post can be updated", async () => {
+    if (testUser === null || postId === null) {
+      expect(false).toBeTruthy();
+      return;
+    } else {
+      const data = {
+        content: "Updated post"
+      };
 
-    expect(response.body.content).toBe("This is a post");
+      const response = await api
+        .patch(`/community/${postId}`)
+        .set("Authorization", `bearer ${testUser.token}`)
+        .send(data)
+        .expect(200);
+
+      expect(response.body.content).toBe("Updated post");
+    }
+  });
+
+  let commentId: string | null = null;
+  test("A comment can be added to a post", async () => {
+    if (testUser === null || postId === null) {
+      expect(false).toBeTruthy();
+      return;
+    } else {
+      const data = {
+        content: "This is a comment"
+      };
+
+      const response = await api
+        .post(`/community/comment/${postId}`)
+        .set("Authorization", `bearer ${testUser.token}`)
+        .send(data)
+        .expect(200);
+
+      expect(response.body.content).toBe("This is a comment");
+      commentId = response.body.id;
+    }
+  });
+
+  test("A comment can be deleted", async () => {
+    if (testUser === null || commentId === null) {
+      expect(false).toBeTruthy();
+      return;
+    } else {
+      await api
+        .delete(`/community/comment/${commentId}`)
+        .set("Authorization", `bearer ${testUser.token}`)
+        .expect(204);
+    }
+  });
+
+  test("A community post can be deleted", async () => {
+    if (testUser === null || postId) {
+      expect(false).toBeTruthy();
+      return;
+    } else {
+      await api
+        .delete(`/community/${postId}`)
+        .set("Authorization", `bearer ${testUser.token}`)
+        .expect(204);
+    }
   });
 });
 
