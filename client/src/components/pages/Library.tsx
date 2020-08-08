32c32c49159ae7d6b8d614c316f0d7cc12f2bf1a
nireo <eemil.lehtikangas1@gmail.com>
2020-08-08 18:36:42 +0300

    improved the visual aspect of the library page

--- client/src/components/pages/Library.tsx
+++ client/src/components/pages/Library.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { connect } from "react-redux";
-import { AppState } from "../../store";
-import Container from "@material-ui/core/Container";
-import { User } from "../../interfaces/User";
-import { Redirect } from "react-router-dom";
-import Typography from "@material-ui/core/Typography";
-import { Video } from "../../interfaces/Video";
-import { getLibraryData } from "../../services/user";
-import { Helmet } from "react-helmet";
-import { VideoEntrySmall } from "../other/VideoEntrySmall";
-import Divider from "@material-ui/core/Divider";
+import React, { useState, useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
+import { AppState } from '../../store';
+import Container from '@material-ui/core/Container';
+import { User } from '../../interfaces/User';
+import { Redirect } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import { Video } from '../../interfaces/Video';
+import { getLibraryData } from '../../services/user';
+import { Helmet } from 'react-helmet';
+import { VideoEntrySmall } from '../other/VideoEntrySmall';
+import Divider from '@material-ui/core/Divider';
 
 type Props = {
   user: User | null;
@@ -45,18 +45,15 @@ const Library: React.FC<Props> = ({ user }) => {
         <title>Library - TypeTube</title>
         <meta charSet="utf-8" />
       </Helmet>
-      <Typography variant="h5" style={{ marginTop: "2rem" }}>
-        History
+      <Typography
+        style={{ marginTop: '2rem', fontSize: '1.1rem', marginBottom: '1rem' }}
+      >
+        <strong>History</strong>
       </Typography>
-      <Divider style={{ marginTop: "1rem", marginBottom: "1rem" }} />
       {historyVideos.length === 0 ? (
         <div>
-          <Typography style={{ marginTop: "1rem" }} variant="h6">
-            No history.
-          </Typography>
-          <Typography>
-            You're history site will be filled with more data, when you watch
-            videos.
+          <Typography variant="body2" color="textSecondary">
+            The videos you watch will display here.
           </Typography>
         </div>
       ) : (
@@ -66,15 +63,21 @@ const Library: React.FC<Props> = ({ user }) => {
           ))}
         </div>
       )}
-      <Typography variant="h5" style={{ marginTop: "2rem" }}>
-        Watch later
+      <Divider style={{ marginTop: '3rem', marginBottom: '1rem' }} />
+      <Typography
+        style={{
+          marginTop: '2rem',
+          fontSize: '1.1rem',
+        }}
+        variant="body1"
+      >
+        <strong>Watch later</strong>
       </Typography>
-      <Divider style={{ marginTop: "1rem", marginBottom: "1rem" }} />
       {watchLaterVideos.length === 0 ? (
         <div>
-          <Typography variant="h6">No watch later videos.</Typography>
-          <Typography>
-            You don't have any videos in the watch later list.
+          <Typography color="textSecondary" variant="body2">
+            You can add videos to your watch later list from the video watch
+            page.
           </Typography>
         </div>
       ) : (
@@ -84,14 +87,18 @@ const Library: React.FC<Props> = ({ user }) => {
           ))}
         </div>
       )}
-      <Typography variant="h5" style={{ marginTop: "2rem" }}>
-        Liked videos
+      <Divider style={{ marginTop: '1rem', marginBottom: '1rem' }} />
+      <Typography
+        style={{
+          fontSize: '1.1rem',
+        }}
+        variant="body1"
+      >
+        <strong>Liked videos</strong>
       </Typography>
-      <Divider style={{ marginTop: "1rem", marginBottom: "1rem" }} />
       {liked.length === 0 ? (
         <div>
-          <Typography variant="h6">No liked videos</Typography>
-          <Typography>
+          <Typography color="textSecondary" variant="body2">
             You haven't liked any videos. This can be done on the video watch
             page!
           </Typography>
@@ -108,7 +115,7 @@ const Library: React.FC<Props> = ({ user }) => {
 };
 
 const mapStateToProps = (state: AppState) => ({
-  user: state.user
+  user: state.user,
 });
 
 export default connect(mapStateToProps)(Library);
